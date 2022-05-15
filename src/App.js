a459e1c58f4c2a936b7f352146f5149b8c19dfcc
Justin Scorzafava <jkscorzafava@gmail.com>
2022-05-15 09:41:24 -0600

    deleted boilerplate app.js and added state values for id, name, room

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './index.css'
 
-function App() {
+function App () {
+  const [id,setId] = useState("")
+  const [nameInput, setNameInput] = useState("");
+  const [room, setRoom ] = useState("")
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
   );
 }
 

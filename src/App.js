40ca5ae5d9db925872e5529fc445c35f22f585ce
Justin Scorzafava <jkscorzafava@gmail.com>
2022-05-15 12:50:54 -0600

    finished different sockets for people-list, add-person, remove-person

--- src/App.js
+++ src/App.js
@@ -1,59 +1,115 @@
-import React, { useState, useEffect } from 'react';
-import useSocket from 'use-socket.io-client';
-import { useImmer } from 'use-immer';
-import './index.css';
+import React, { useState, useEffect } from "react";
+import useSocket from "use-socket.io-client";
+import { useImmer } from "use-immer";
+
+import "./index.css";
 
 const Messages = (props) =>
   props.data.map((m) =>
-    m[0] !== '' ? (
-      <li key={m[0]}>
+    m[0] !== "" ? (
+      <li>
         <strong>{m[0]}</strong> : <div className='innermsg'>{m[1]}</div>
       </li>
     ) : (
-      <li key={m[1]} className='update'>
-        {m[1]}
-      </li>
+      <li className='update'>{m[1]}</li>
     )
   );
 
-function App() {
-  const [id, setId] = useState('');
-  const [nameInput, setNameInput] = useState('');
-  const [room, setRoom] = useState('');
-  const [socket] = useSocket('<https://open-chat-naostsaecf.now.sh>');
+const Online = (props) => props.data.map((m) => <li id={m[0]}>{m[1]}</li>);
 
+export default () => {
+  const [id, setId] = useState("");
+  const [nameInput, setNameInput] = useState("");
+  const [room, setRoom] = useState("");
+  const [input, setInput] = useState("");
+
+  const [socket] = useSocket("https://open-chat-naostsaecf.now.sh");
   socket.connect();
 
   const [messages, setMessages] = useImmer([]);
   const [online, setOnline] = useImmer([]);
 
   useEffect(() => {
-    socket.on('message que', (nick, message) => {
+    socket.on("message que", (nick, message) => {
       setMessages((draft) => {
         draft.push([nick, message]);
       });
     });
 
-socket.on('update',message => setMessages(draft => {
-  draft.push(['',message])
-}))
+    socket.on("update", (message) =>
+      setMessages((draft) => {
+        draft.push(["", message]);
+      })
+    );
+
+    socket.on("people-list", (people) => {
+      let newState = [];
+      for (let person in people) {
+        newState.push([people[person].id, people[person].nick]);
+      }
+      setOnline((draft) => {
+        draft.push(...newState);
+      });
+      console.log(online);
+    });
+
+    socket.on("add-person", (nick, id) => {
+      setOnline((draft) => {
+        draft.push([id, nick]);
+      });
+    });
+
+    socket.on("remove-person", (id) => {
+      setOnline((draft) => draft.filter((m) => m[0] !== id));
+    });
+
+    socket.on("chat message", (nick, message) => {
+      setMessages((draft) => {
+        draft.push([nick, message]);
+      });
+    });
+  }, 0);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!nameInput) {
       return alert("Name can't be empty");
     }
-    setId(firstName);
-    socket.emit('join', firstName, room);
+    setId(window.name);
+    socket.emit("join", window.name, room);
   };
 
-  return id !== '' ? (
-    <div>Hello</div>
+  const handleSend = (e) => {
+    e.preventDefault();
+    if (input !== "") {
+      socket.emit("chat message", input, room);
+      setInput("");
+    }
+  };
+
+  return id ? (
+    <section style={{ display: "flex", flexDirection: "row" }}>
+      <ul id='messages'>
+        <Messages data={messages} />
+      </ul>
+      <ul id='online'>
+        {" "}
+        &#x1f310; : <Online data={online} />{" "}
+      </ul>
+      <div id='sendform'>
+        <form onSubmit={(e) => handleSend(e)} style={{ display: "flex" }}>
+          <input id='m' onChange={(e) => setInput(e.target.value.trim())} />
+          <button style={{ width: "75px" }} type='submit'>
+            Send
+          </button>
+        </form>
+      </div>
+    </section>
   ) : (
-    <section style={{ textAlign: 'center', margin: '30vh auto', width: '70%' }}>
+    <div style={{ textAlign: "center", margin: "30vh auto", width: "70%" }}>
       <form onSubmit={(event) => handleSubmit(event)}>
         <input
-          id='firstName'
+          id='name'
           onChange={(e) => setNameInput(e.target.value.trim())}
           required
           placeholder='What is your name ..'
@@ -67,8 +123,6 @@ socket.on('update',message => setMessages(draft => {
         <br />
         <button type='submit'>Submit</button>
       </form>
-    </section>
+    </div>
   );
-}
-
-export default App;
+};

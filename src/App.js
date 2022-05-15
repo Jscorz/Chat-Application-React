c82e3f27eef9b0f6db945463b4cf9b94ded717a1
Justin Scorzafava <jkscorzafava@gmail.com>
2022-05-15 10:47:01 -0600

    changed name to firstName and changed react dom setup in index.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { useState } from 'react';
+import useSocket from 'use-socket.io-client';
 import './index.css';
 
 function App() {
   const [id, setId] = useState('');
   const [nameInput, setNameInput] = useState('');
   const [room, setRoom] = useState('');
+  const [socket] = useSocket('<https://open-chat-naostsaecf.now.sh>');
+
+  socket.connect();
+  console.log(socket);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!nameInput) {
       return alert("Name can't be empty");
     }
-    setId(name);
-    socket.emit('join', name, room);
+    setId(firstName);
+    socket.emit('join', firstName, room);
   };
 
   return id !== '' ? (
@@ -21,7 +26,7 @@ function App() {
     <section style={{ textAlign: 'center', margin: '30vh auto', width: '70%' }}>
       <form onSubmit={(event) => handleSubmit(event)}>
         <input
-          id='name'
+          id='firstName'
           onChange={(e) => setNameInput(e.target.value.trim())}
           required
           placeholder='What is your name ..'

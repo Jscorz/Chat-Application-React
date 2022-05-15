a7c98b6ac9a663ac772ad4efa356d242a832e91d
Justin Scorzafava <jkscorzafava@gmail.com>
2022-05-15 11:31:18 -0600

    added useEffect for message que to mutate state while preserving old state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import useSocket from 'use-socket.io-client';
+import { useImmer } from 'use-immer';
 import './index.css';
+
+const Messages = (props) =>
+  props.data.map((m) =>
+    m[0] !== '' ? (
+      <li key={m[0]}>
+        <strong>{m[0]}</strong> : <div className='innermsg'>{m[1]}</div>
+      </li>
+    ) : (
+      <li key={m[1]} className='update'>
+        {m[1]}
+      </li>
+    )
+  );
 
 function App() {
   const [id, setId] = useState('');
   const [nameInput, setNameInput] = useState('');
   const [room, setRoom] = useState('');
   const [socket] = useSocket('<https://open-chat-naostsaecf.now.sh>');
-  const [data, setData] = useImmer(default_value);
 
   socket.connect();
-  console.log(socket);
+
+  const [messages, setMessages] = useImmer([]);
+  const [online, setOnline] = useImmer([]);
+
+  useEffect(() => {
+    socket.on('message que', (nick, message) => {
+      setMessages((draft) => {
+        draft.push([nick, message]);
+      });
+    });
+
+    socket.on('update', (message) => {
+      setMessages((draft) => {
+        draft.push(['', message]);
+      })
+  });
 
   const handleSubmit = (e) => {
     e.preventDefault();
